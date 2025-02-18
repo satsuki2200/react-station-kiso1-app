0ef01046d9b7c308a7cab048661c3fe99861cd3b
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2025-02-19 00:59:38 +0900

    chore: run code formatting and linting

--- src/threads/NewThread.jsx
+++ src/threads/NewThread.jsx
@@ -4,59 +4,59 @@ import "./NewThread.css";
 import Header from "../Header";
 
 const NewThread = () => {
-  const [title, setTitle] = useState("");
+    const [title, setTitle] = useState("");
 
-  const handleChange = (e) => {
-    setTitle(e.target.value);
-    console.log(title);
-  };
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+        console.log(title);
+    };
 
-  const handleSubmit = async () => {
-    console.log(title);
+    const handleSubmit = async () => {
+        console.log(title);
 
-    const response = await fetch(
-      "https://railway.bulletinboard.techtrain.dev/threads",
-      {
-        method: "POST",
-        body: JSON.stringify({ title: title }),
-      },
+        const response = await fetch(
+            "https://railway.bulletinboard.techtrain.dev/threads",
+            {
+                method: "POST",
+                body: JSON.stringify({ title: title }),
+            }
+        );
+
+        if (response.ok) {
+            console.log("success");
+            return response.json();
+        } else if (response.status === 400) {
+            console.log("400");
+            console.log(response);
+            return response.json();
+        }
+    };
+    return (
+        <>
+            <Header />
+            <h2>スレッド新規作成</h2>
+
+            <form onSubmit={() => handleSubmit()}>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    size="25"
+                    placeholder="スレッドタイトル"
+                    required
+                    onChange={(e) => handleChange(e)}
+                />
+                <div className="form-navigation">
+                    <Link to="/">
+                        <p className="go-top">Topに戻る</p>
+                    </Link>
+                    <button type="submit" className="submit">
+                        作成
+                    </button>
+                </div>
+            </form>
+        </>
     );
-
-    if (response.ok) {
-      console.log("success");
-      return response.json();
-    } else if (response.status === 400) {
-      console.log("400");
-      console.log(response);
-      return response.json();
-    }
-  };
-  return (
-    <>
-      <Header />
-      <h2>スレッド新規作成</h2>
-
-      <form onSubmit={() => handleSubmit()}>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          size="25"
-          placeholder="スレッドタイトル"
-          required
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="form-navigation">
-          <Link to="/">
-            <p className="go-top">Topに戻る</p>
-          </Link>
-          <button type="submit" className="submit">
-            作成
-          </button>
-        </div>
-      </form>
-    </>
-  );
 };
 
 export default NewThread;
