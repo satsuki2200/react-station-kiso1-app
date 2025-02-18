112eafd7c04f638265cfe66adcbefbd73fe442d1
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2025-02-18 18:46:25 +0900

    feat: get thread list

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [threads, setThreads] = useState([]);
+
+    useEffect(() => {
+        console.log("useEffect");
+        const fetchData = async () => {
+            try {
+                const res = await fetch(
+                    "https://railway.bulletinboard.techtrain.dev/threads",
+                    { method: "GET" }
+                );
+                const data = await res.json();
+                setThreads(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (threads.length === 0) {
+        return <h1>Loading...</h1>;
+    }
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <header>掲示板</header>
+            <h2>スレッド一覧</h2>
+            {console.log(threads)}
+            {threads.map((thread) => {
+                return (
+                    <div key={thread.id} className="thread">
+                        <h3>{thread.title}</h3>
+                    </div>
+                );
+            })}
         </>
     );
 }
