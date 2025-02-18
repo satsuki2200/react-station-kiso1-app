a35e1643d5ea7a6bcd2ab368e8884d9106765302
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2025-02-19 00:58:43 +0900

    chore: change file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./Home";
-import New from "./threads/New";
+import NewThread from "./threads/NewThread";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/threads/new" element={<New />} />
+        <Route path="/threads/new" element={<NewThread />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <Link to="/">
         <p className="title">掲示板</p>
       </Link>
-      <Link to="/threads/new" className="newThreadButton">
+      <Link to="/threads/new" className="NewButton">
         <button>スレッド作成</button>
       </Link>
     </div>
