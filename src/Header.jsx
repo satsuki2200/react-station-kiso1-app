4f39211552660aeaf5851b7cf4612ac25017de6a
satsuki2200 <132316826+satsuki2200@users.noreply.github.com>
2025-02-20 18:33:16 +0900

    chore: run code formatting and linting

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <Link to="/">
-        <p className="title">掲示板</p>
-      </Link>
-      <Link to="/threads/new" className="NewButton">
-        <button>スレッド作成</button>
-      </Link>
-    </div>
-  );
+    return (
+        <div className="header">
+            <Link to="/">
+                <p className="title">掲示板</p>
+            </Link>
+            <Link to="/threads/new" className="NewButton">
+                <button>スレッド作成</button>
+            </Link>
+        </div>
+    );
 };
 
 export default Header;
